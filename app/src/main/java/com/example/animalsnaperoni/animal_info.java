0b52724efd82ba9463fdec752ed6a65b1a2ee0ab
ItsJJH <69231931+ItsJJH@users.noreply.github.com>
2022-03-25 12:25:21 -0400

    revamp to main

---
+++ app/src/main/java/com/example/animalsnaperoni/animal_info.java
@@ -0,0 +1,14 @@
+package com.example.animalsnaperoni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class animal_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_info);
+    }
+}
