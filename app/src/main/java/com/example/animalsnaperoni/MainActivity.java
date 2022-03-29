656eefa1fd1ce4eb6c29810d83644292a76114fe
Amanuel <92159492+amanuelak@users.noreply.github.com>
2022-03-28 21:14:38 -0400

    Recreated the info page

--- app/src/main/java/com/example/animalsnaperoni/MainActivity.java
+++ app/src/main/java/com/example/animalsnaperoni/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.animalsnaperoni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton myImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       myImageButton = (ImageButton) findViewById(R.id.germanShepherd);
+
+
+       myImageButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intentChangePage = new Intent(MainActivity.this, animal_info.class);
+               startActivity(intentChangePage);
+           }
+       });
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/animalsnaperoni/animal_info.java
+++ app/src/main/java/com/example/animalsnaperoni/animal_info.java
@@ -1,14 +1,141 @@
 package com.example.animalsnaperoni;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
 
 public class animal_info extends AppCompatActivity {
+
+
+    ImageButton backButton;
+
+    private static int REQUEST_CODE = 100;
+    ImageView dog;
+    Button downloadImage;
+    OutputStream outputStream;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal_info);
+
+
+
+        backButton = (ImageButton) findViewById(R.id.backHome);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goBack = new Intent(animal_info.this, MainActivity.class);
+                startActivity(goBack);
+
+            }
+        });
+
+        downloadImage = findViewById(R.id.downloadButton);
+        dog = findViewById(R.id.germanShepherd);
+        downloadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(animal_info.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                {
+                    downloadImg();
+                }
+                else {
+                    askPermission();
+                }
+            }
+
+
+        });
+
+
+
+
+
+
     }
+
+    private void askPermission() {
+
+        ActivityCompat.requestPermissions(animal_info.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                downloadImg();
+            }
+            else
+            {
+                Toast.makeText(animal_info.this, "Please provide the permissions", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void downloadImg() {
+
+        File dir = new File(Environment.getExternalStorageDirectory(), "SaveImage");
+
+        if (!dir.exists())
+        {
+            dir.mkdir();
+        }
+
+        BitmapDrawable drawable =(BitmapDrawable) dog.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        File file = new File(dir, System.currentTimeMillis() + ".jpg");
+        try {
+            outputStream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        Toast.makeText(animal_info.this, "Saved", Toast.LENGTH_SHORT).show();
+
+        try {
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
